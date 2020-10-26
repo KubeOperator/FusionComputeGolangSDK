56e9be3684b5fd6ec765f133618375ff66f6794b
chenyang <907693148@qq.com>
2020-10-26 17:46:32 +0800

    增加子网掩码转换

--- pkg/vm/vm.go
+++ pkg/vm/vm.go
@@ -1,10 +1,14 @@
 package vm
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/KubeOperator/FusionComputeGolangSDK/pkg/client"
 	"github.com/KubeOperator/FusionComputeGolangSDK/pkg/common"
 	"path"
+	"strconv"
 	"strings"
 )
 
@@ -30,6 +34,20 @@ type manager struct {
 }
 
 func (m *manager) CloneVm(templateUri string, request CloneVmRequest) (*CloneVmResponse, error) {
+	for n := range request.VmCustomization.NicSpecification {
+		if !strings.Contains(request.VmCustomization.NicSpecification[n].Netmask, ".") {
+			b, err := strconv.Atoi(request.VmCustomization.NicSpecification[0].Netmask)
+			if err != nil {
+				return nil, errors.New(fmt.Sprintf("can not parse netmask: %s", err.Error()))
+			}
+			mask, err := parseMask(b)
+			if err != nil {
+				return nil, errors.New(fmt.Sprintf("can not parse netmask: %s", err.Error()))
+			}
+			request.VmCustomization.NicSpecification[n].Netmask = mask
+		}
+	}
+
 	var cloneVmResponse CloneVmResponse
 	api, err := m.client.GetApiClient()
 	if err != nil {
@@ -120,4 +138,21 @@ func (m *manager) GetVM(vmUri string) (*Vm, error) {
 		return nil, common.FormatHttpError(resp)
 	}
 	return &item, nil
+}
+
+func parseMask(num int) (mask string, err error) {
+	var buff bytes.Buffer
+	for i := 0; i < int(num); i++ {
+		buff.WriteString("1")
+	}
+	for i := num; i < 32; i++ {
+		buff.WriteString("0")
+	}
+	masker := buff.String()
+	a, _ := strconv.ParseUint(masker[:8], 2, 64)
+	b, _ := strconv.ParseUint(masker[8:16], 2, 64)
+	c, _ := strconv.ParseUint(masker[16:24], 2, 64)
+	d, _ := strconv.ParseUint(masker[24:32], 2, 64)
+	resultMask := fmt.Sprintf("%v.%v.%v.%v", a, b, c, d)
+	return resultMask, nil
 }
